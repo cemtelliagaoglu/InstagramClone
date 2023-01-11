5c48a5b844416851efb432c12e77b81526a06675
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-01-12 00:20:36 +0300

    add FeedCellDelegate Protocol and ui improvements

--- InstagramClone/Controller/Feed/FeedVC.swift
+++ InstagramClone/Controller/Feed/FeedVC.swift
@@ -7,50 +7,70 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseDatabase
 
 private let reuseIdentifier = "Cell"
 
-class FeedVC: UICollectionViewController {
+class FeedVC: UICollectionViewController,UICollectionViewDelegateFlowLayout {
 
     //MARK: - Properties
+    
+    var posts = [Post]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        self.collectionView!.register(FeedCell.self, forCellWithReuseIdentifier: reuseIdentifier)
         
-        configureLogOutButton()
+        collectionView.backgroundColor = .white
+        
+        configureNavController()
+        
+        fetchPosts()
     }
 
-
+    //MARK: - UICollectionViewFlowLayout
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = view.frame.width
+        var height = width + 8 + 40 + 8
+        height += 50 // actionButtons at bottom
+        height += 60
+        return CGSize(width: width, height: height)
+    }
 
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return posts.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! FeedCell
+        cell.post = posts[indexPath.row]
+        cell.delegate = self
         // Configure the cell
     
         return cell
     }
 
     //MARK: - Handlers
-    func configureLogOutButton(){
-
+    func configureNavController(){
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
+        self.navigationItem.title = "Feed"
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "send2"), style: .plain, target: self, action: #selector(handleShowMessages))
     }
-
+    
+    @objc func handleShowMessages(){
+        print("handle show messages")
+    }
     @objc func handleLogout(){
         // declare alert controller
         let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
@@ -69,5 +89,55 @@ class FeedVC: UICollectionViewController {
         alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         present(alertController,animated: true)
     }
+    //MARK: - API
+    
+    func fetchPosts(){
+        POSTS_REF.observe(.childAdded) { snapshot in
+            let postId = snapshot.key
+            
+            guard let dictionary = snapshot.value as? Dictionary<String,AnyObject> else{ return }
+            guard let ownerUid = dictionary["ownerUid"] as? String else{ return }
+            
+            Database.fetchUser(with: ownerUid) { user in
+                
+                let post = Post(postId: postId,user: user, dictionary: dictionary)
+                
+                Database.fetchPost(with: postId) { post in
+                    
+                    self.posts.append(post)
+                    
+                    self.posts.sort(by: {$0.creationDate > $1.creationDate})
+                    
+                    self.collectionView.reloadData()
+                }
+            }
+        }
+    }
 
+}
+//MARK: - FeedCellDelegate Protocol
+extension FeedVC: FeedCellDelegate{
+    
+    func handleUsernameTapped(for cell: FeedCell) {
+        let userProfileVC = UserProfileVC(collectionViewLayout: UICollectionViewFlowLayout())
+        guard let user = cell.post?.user else{ return }
+        userProfileVC.user = user
+        
+        navigationController?.pushViewController(userProfileVC, animated: true)
+        
+    }
+    
+    func handleOptionsTapped(for cell: FeedCell) {
+        print("handle options tapped")
+    }
+    
+    func handleLikeTapped(for cell: FeedCell) {
+        print("handle like tapped")
+    }
+    
+    func handleCommentTapped(for cell: FeedCell) {
+        print("handle comment tapped")
+    }
+    
+    
 }
